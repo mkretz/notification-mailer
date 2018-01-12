45365005df1cffb797edf1fc65220bc79050bc8b
Kretz Mathis <mathis.kretz@swisscom.com>
2018-01-12 23:26:32 +0100

    send email on message

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ var appEnv = cfenv.getAppEnv();
 var rabbitmqUri = appEnv.getServiceURL('notification-mq');
 var smtpUri = appEnv.getServiceURL('notification-mail');
 var amqp = require('amqplib/callback_api');
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport(smtpUri);
+
+function sendMail(subject, message) {
+    var sendingAddress = appEnv.getServiceCreds('notification-mail').toAddress;
+    var mailOptions = {
+        to: sendingAddress,
+        subject: subject,
+        text: message
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
+}
 
 amqp.connect(rabbitmqUri, function(err, conn) {
     conn.createChannel(function(err, ch) {
@@ -10,7 +27,9 @@ amqp.connect(rabbitmqUri, function(err, conn) {
         ch.assertQueue(queue, {durable: false});
         console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
         ch.consume(queue, function(msg) {
-                console.log(JSON.parse(msg.content));
+                var message = JSON.parse(msg.content);
+                console.log(message);
+                sendMail("Notification!", "Hi, here's a new notification: " + message.text);
             });
         }, {noAck: true});
     });
