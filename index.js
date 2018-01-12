1e03468042095290f455f3d5b139982d99314d24
Kretz Mathis <mathis.kretz@swisscom.com>
2018-01-12 22:17:45 +0100

    set up ampqp queue consumer

---
+++ index.js
@@ -0,0 +1,16 @@
+var cfenv = require('cfenv');
+var appEnv = cfenv.getAppEnv();
+var rabbitmqUri = appEnv.getServiceURL('notification-mq');
+var smtpUri = appEnv.getServiceURL('notification-mail');
+var amqp = require('amqplib/callback_api');
+
+amqp.connect(rabbitmqUri, function(err, conn) {
+    conn.createChannel(function(err, ch) {
+        var queue = "notifications";
+        ch.assertQueue(queue, {durable: false});
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+        ch.consume(queue, function(msg) {
+
+            });
+        }, {noAck: true});
+    });
